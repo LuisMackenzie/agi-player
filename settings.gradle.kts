7b504205946b7eed77af82cac31fc6281f7c7bf6
mackenzie <principemackenzie@gmail.com>
2026-03-08 14:06:41 +0100

    project name and added resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -31,7 +34,7 @@ dependencyResolutionManagement {
 //    }
 //}
 
-rootProject.name = "NextPlayer"
+rootProject.name = "AGI Player"
 include(":app")
 include(":core:common")
 include(":core:data")
